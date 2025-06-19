6bf81fcb1a000534d6acaa68dbf75cef6a973fe5
Keaton Murray <murraykeaton422@gmail.com>
2025-06-18 21:13:45 -0400

    Created Custom Middleware to Prevent Unauthenticated Users From Accessing the Inventory Page Directly

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 // Product Controller Routes
 Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::get('/your-inventory', [ProductController::class, 'create'])->name('create');
+Route::get('/your-inventory', [ProductController::class, 'create'])->name('create') ->middleware('custom.auth');
 Route::get('/fetch-products', [ProductController::class, 'fetch'])->name('fetch');
 Route::get('/products/fetch-blade', [ProductController::class, 'fetchBlade'])->name('fetch.blade');
 Route::post('/save-product', [ProductController::class, 'store'])->name('store');
